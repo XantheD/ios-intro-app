b5504964edee774bdc0fe38766a2d1bfeb2bbcdb
Xanthe <xanthe.dobbie@gmail.com>
2015-04-28 13:55:52 +1000

    Initial setup ToDoList
    First stages to commit

---
+++ ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Elizabeth Mitchell on 27/04/2015.
+//  Copyright (c) 2015 Xanthe Dobbie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Elizabeth Mitchell on 27/04/2015.
+//  Copyright (c) 2015 Xanthe Dobbie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
